f1f8a4822baff966ccc414538634a0f6e71b7418
lavan <lavan@DESKTOP-E9U7U8V>
2018-10-05 09:49:28 -0400

    Commiting

---
+++ src/Package1/Class1.java
@@ -0,0 +1,19 @@
+package Package1;
+
+public class Class1 {
+	
+	public void method1()
+	{
+		for(int i=20;i>=1;i--)
+		{
+			System.out.println(i);
+		}
+	}
+	
+	public static void main(String[] args)
+	{
+		Class1 obj = new Class1();
+		obj.method1();
+	}
+
+}
